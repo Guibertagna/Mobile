eef05c6136117cf8cca4a0ea8caac7d5a7c6061c
Guibertagna <guirogeriobertagna00@gmail.com>
2023-10-25 23:36:27 -0300

    calculadora final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,88 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  visor : number = 0;
-  constructor() {}
+  visor: string = '0';
+  numeros: string[] = [];
+  operadores: string[] = [];
 
+  adicionarNumero(valor: string) {
+    if (this.numeros.length === 0) {
+      this.numeros.push(valor);
+    } else if (this.operadores.length < this.numeros.length) {//ele verifica se o array de numeros é menor que o array de operações
+      this.numeros[this.numeros.length - 1] += valor;// se menor ele contatena o numero clicado a posição anterior do vetor
+    } else {
+      this.numeros.push(valor);
+    }
+    this.atualizarVisor();
+  }
+
+  adicionarOperacao(valor: string) {
+    if (this.numeros.length > 0) {
+      this.operadores.push(valor);
+      this.atualizarVisor();
+    }
+  }
+
+  inverter() {
+    if (this.numeros.length > 0) {
+        this.numeros[this.numeros.length - 1] = (parseFloat(this.numeros[this.numeros.length - 1]) * -1).toString();
+      this.atualizarVisor();
+    } 
+  }
+  
+  porcentagem() {
+    if (this.numeros.length > 0) {
+      this.numeros[this.numeros.length - 1] = (parseFloat(this.numeros[this.numeros.length - 1]) / 100).toString();
+      this.atualizarVisor();
+    }
+  }
+
+  calcular() {
+    if (this.numeros.length >= 2 && this.operadores.length >= 1) {
+      let resultado = parseFloat(this.numeros[0]);
+      for (let i = 0; i < this.operadores.length; i++) {
+        const num2 = parseFloat(this.numeros[i + 1]);
+        const operador = this.operadores[i];
+
+        if (!isNaN(num2)) {
+          switch (operador) {
+            case '+':
+              resultado += num2;
+              break;
+            case '-':
+              resultado -= num2;
+              break;
+            case 'x':
+              resultado *= num2;
+              break;
+            case '÷':
+              resultado /= num2;
+              break;
+          }
+        }
+      }
+      this.numeros = [resultado.toString()];
+      this.operadores = [];
+      this.atualizarVisor();
+    }
+  }
+
+  zerar() {
+    this.visor = '0';
+    this.numeros = [];
+    this.operadores = [];
+  }
+
+  atualizarVisor() {
+    let expressao = '';
+    for (let i = 0; i < this.numeros.length; i++) {
+      expressao += this.numeros[i];
+      if (i < this.operadores.length) {
+        expressao += ' ' + this.operadores[i] + ' ';
+      }
+    }
+    this.visor = expressao;
+  }
+
+  constructor() {}
 }
