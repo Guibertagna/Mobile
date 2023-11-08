db524afa8345d8965c6fb64e5fbeaa314d341e58
Guibertagna <guirogeriobertagna00@gmail.com>
2023-11-07 21:56:30 -0300

    pagina details feita,faltando listar personagens

--- ApiRickAndMorty/src/app/view/busca-details/busca-details.page.ts
+++ ApiRickAndMorty/src/app/view/busca-details/busca-details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RickAndMortyServiceService, SearchType } from 'src/app/service/rick-and-morty-service.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-busca-details',
@@ -10,15 +11,17 @@ import { RickAndMortyServiceService, SearchType } from 'src/app/service/rick-and
 export class BuscaDetailsPage implements OnInit {
   info: any;
 
-  constructor(private actRout: ActivatedRoute, private rickapi: RickAndMortyServiceService) { }
 
+  constructor(private actRout: ActivatedRoute, private rickapi: RickAndMortyServiceService,  private http: HttpClient) { }
+  
   ngOnInit() {
     const id = this.actRout.snapshot.paramMap.get('id');
     const type = this.actRout.snapshot.paramMap.get('type') as SearchType; 
 
     this.rickapi.getById(id, type).subscribe(result => this.info = result);
   }
-
+  
+  
   openURL(){
     window.open(this.info.Website,'_blank')
   }
