e93a240f940b60fec97337a55f04fd6c2d2cdbc1
Guibertagna <guirogeriobertagna00@gmail.com>
2023-11-16 09:28:50 -0300

    app de contatos

---
+++ contatos-TA/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCSoF0niKL9QNGrteG6hY6NcIXDkb0DziA",
+    authDomain: "app-contato-a3be1.firebaseapp.com",
+    projectId: "app-contato-a3be1",
+    storageBucket: "app-contato-a3be1.appspot.com",
+    messagingSenderId: "728520583738",
+    appId: "1:728520583738:web:4fea148d137996e2f6e303",
+    measurementId: "G-YSN07SNQZP"
+  }
+};
