4b7c0ee82f26d255aaf1fc7d59e5278f80bb92ab
Guibertagna <guirogeriobertagna00@gmail.com>
2023-11-17 15:36:21 -0300

    apk da api completo final

--- ApiRickAndMorty/src/app/view/busca-details/busca-details.page.ts
+++ ApiRickAndMorty/src/app/view/busca-details/busca-details.page.ts
@@ -10,19 +10,32 @@ import { HttpClient } from '@angular/common/http';
 })
 export class BuscaDetailsPage implements OnInit {
   info: any;
+  episodes: any[] = [];
+  episodesToShow: number = 4;
+  showMoreEpisodes() {
+    this.episodesToShow += 4; // Aumenta o número de episódios a serem exibidos
+  }
 
+  constructor(private actRout: ActivatedRoute, private rickapi: RickAndMortyServiceService, private http: HttpClient) {}
 
-  constructor(private actRout: ActivatedRoute, private rickapi: RickAndMortyServiceService,  private http: HttpClient) { }
-  
   ngOnInit() {
     const id = this.actRout.snapshot.paramMap.get('id');
-    const type = this.actRout.snapshot.paramMap.get('type') as SearchType; 
-
-    this.rickapi.getById(id, type).subscribe(result => this.info = result);
+    const type = this.actRout.snapshot.paramMap.get('type') as SearchType;
+  
+    this.rickapi.getById(id, type).subscribe(result => {
+      this.info = result;
+      if (Array.isArray(this.info.episode) && this.info.episode.length > 0) {
+        this.info.episode.forEach((episodeLink: any) => {
+          this.http.get(episodeLink).subscribe((episodeDetails: any) => {
+            this.episodes.push(episodeDetails);
+          });
+        });
+      }
+    });
   }
   
-  
-  openURL(){
-    window.open(this.info.Website,'_blank')
+
+  openURL() {
+    window.open(this.info.Website, '_blank');
   }
 }
